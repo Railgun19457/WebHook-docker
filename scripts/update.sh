3f801a2255cc2d8b30b8b939a6e447bf57d7e90f
Misaka Mikoto <117180744+Railgun19457@users.noreply.github.com>
2026-04-19 20:20:38 +0800

    Delete scripts directory

--- scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo "[$(date -Iseconds)] update script started"
-# Replace with real update logic, such as git pull and build.
-echo "[$(date -Iseconds)] update script finished"
